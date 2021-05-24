4fa4f1c8339e659ca54941f1e2a7e7118174ae05
Cristina <cristinaponce.designer@gmail.com>
2021-05-24 20:01:34 +0200

    chore: improve tests for View.py

--- view/TestView.py
+++ view/TestView.py
@@ -4,8 +4,6 @@ from unittest.mock import patch
 from model.Geometria import Geometria
 from view.View import View
 
-figuras = ['Cuadrado', 'Circulo', 'Triangulo', 'Rectangulo', 'Pentagono', 'Rombo', 'Romboide', 'Trappecio']
-areas = [4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0]
 opciones = ['1', '2', '3', '4', '5', '6', '7', '8']
 
 
@@ -18,19 +16,8 @@ class TestView(unittest.TestCase):
         area = v.select(g)
         figura = g.figuraName
 
-        # Así funciona
         self.assertEqual(figura, 'Cuadrado')
         self.assertEqual(area, 4.0)
-
-        # Pero yo quiero hacerlo dinámico
-        # Algo así, con un index
-        # self.assertEqual(figura, figuras[index])
-        # self.assertEqual(area, areas[index])
-
-
-for figura in figuras:
-    test = TestView()
-    result = test.test_select()
 
 
 if __name__ == '__main__':
